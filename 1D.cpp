85d83033f8090e86b5c4adf9c8118990cc9cea77
pavelleckaya <zubov.ps@phystech.edu>
2021-11-15 12:11:08 +0300

    Зубов Павел Б05-125, 1 контест, задача D

---
+++ 1D.cpp
@@ -0,0 +1,58 @@
+    #include <iostream>
+    #include <queue>
+    #include <stack>
+    #include <deque>
+     
+    class Queuegob {
+    	std::deque<int> queue1, queue2;
+    public:
+    	void Push_priviliged_gob(int numgob) {
+    		if (queue1.size() == queue2.size()) {
+    			queue1.push_back(numgob);
+    		}
+    		else {
+    			queue2.push_front(numgob);
+    		}
+    	}
+    	void Push__normal_gob(int numgob) {
+    		queue2.push_back(numgob);
+    		if (queue1.size() < queue2.size()) {
+    			queue1.push_back(queue2.front());
+    			queue2.pop_front();
+    		}
+    		
+    	}
+    	void Pop_gob() {
+    		std::cout << queue1.front() << '\n';
+    		queue1.pop_front();
+    		if (queue1.size() < queue2.size()) {
+    			queue1.push_back(queue2.front());
+    			queue2.pop_front();
+    		}
+    	}
+    };
+     
+    int main() {
+    	std::ios::sync_with_stdio(false);
+    	std::cin.tie(NULL);
+    	Queuegob och;
+    	int n;
+    	char znach;
+    	std::cin >> n;
+    	for (int i = 0;i < n;++i) {
+    		std::cin >> znach;
+    		if (znach == '*') {
+			int numgob;
+			std::cin >> numgob;
+    			och.Push_priviliged_gob(numgob);	
+    		}
+    		else if (znach == '-') {
+    			och.Pop_gob();
+    		}
+    		else if (znach == '+') {
+    			int numgob;
+			std::cin >> numgob;
+			och.Push__normal_gob(numgob);	
+    		}
+    	}
+    }
