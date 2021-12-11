df085e4252335c8d5d76569b666beed3bde555bf
pavelleckaya <zubov.ps@phystech.edu>
2021-12-11 22:19:06 +0300

    Зубов Павел Б05-125

--- 2H.cpp
+++ 2H.cpp
@@ -2,41 +2,51 @@
 #include <cmath>
 #include <vector>
 
-int main(){
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(0);
-    std::cout.tie(0);
-    int n;
-    std::cin >> n;
-    std::vector<std::vector<int> > LSD(n, std::vector<int> (8));
-    for(int i = 0; i < n; ++i){
-        long long num;
-	std::cin >> num;
-        for(int j = 0; j < 8; ++j){
-	    LSD[i][j] = num % 256;
-	    num /= 256; 
+int element_LSD(std::vector<long long>& LSD, int j, int i){
+	long long num = LSD[i];
+	for(int z = 0; z < j; ++z){
+		num /= 256;
 	}
-    }
-    for(int j = 0; j < 8; ++j){
-	std::vector<std::vector<int> > ans(n, std::vector<int> (8));
-        std::vector<int> cnt(256);
+	return num % 256;
+
+}
+
+void LSD_sort(std::vector<long long>& LSD, int n){ 
+	int def = 256;
+	int size = 8;
+    for(int j = 0; j < size; ++j){
+		std::vector<long long> ans(n);
+        std::vector<int> cnt(def);
         for(int i = 0; i < n; ++i){
-	    cnt[LSD[i][j]]++;
-        } 
-        for(int i = 1; i < 256; ++i){
+      		int k = element_LSD(LSD, j, i);
+			cnt[k]++;			
+		}
+        for(int i = 1; i < def; ++i){
             cnt[i] += cnt[i-1];
         }
     
         for(int i = n - 1; i >= 0; --i){
-	    ans[--cnt[LSD[i][j]]] = LSD[i];
-	}
-	LSD = ans;
+	    	int k = element_LSD(LSD, j, i);
+			ans[--cnt[k]] = LSD[i];
+		}
+		LSD = ans;
     }
-    for(int j = 0; j < n; ++j){
-	long long num = 0;
-        for(int i = 0; i < 8; ++i){
-	    num += (LSD[j][i] * (std::pow(256,i)));     
+}
+
+int main(){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+	int n;
+	std::cin >> n;
+	std::vector<long long> LSD;
+	for(int i = 0; i < n; ++i){
+		long long num;
+		std::cin >> num;
+		LSD.push_back(num);
 	}
-	std::cout << num << ' ';
-    }
+	LSD_sort(LSD, n);
+	for(int j = 0; j < n; ++j){
+		std::cout << LSD[j] << ' ';
+	}
 }
