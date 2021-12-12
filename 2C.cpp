21f6300d2d8ae617fc749fe7a1a4a54e6577ab1c
pavelleckaya <zubov.ps@phystech.edu>
2021-12-12 12:40:07 +0300

    Зубов Павел Б05-125

--- 2C.cpp
+++ 2C.cpp
@@ -2,59 +2,101 @@
 #include <vector>
 #include <algorithm>
 
-std::vector<int> kth(std::vector<int>& A){
-    std::vector<int> midl;
+int med(std::vector<int>& midl, int size){
+	for(int j = 0; j < size - 1; ++j){
+		for(int i = j + 1; i < size; ++i){
+			if(midl[j] > midl[i]){
+				std::swap(midl[i], midl[j]);
+			}
+		}
+	}
+	return midl[size/2];
+}
+
+int kth(std::vector<int>& A,int left, int right){
+    if(right - left == 0){
+		return A[0];
+	}
+	int size = right - left + 1;
+	std::vector<int> midl;
     std::vector<int> five_el(5);
-    int remains = A.size() / 5, the_whole_part = (A.size() % 5) ;
+    int remains = size / 5, the_whole_part = (size % 5) ;
     for(int i = 0; i < remains; ++i){
         for(int j = 0; j < 5; ++j){
-            five_el[j] = A[5 * i + j];
-    }
-        sort(five_el.begin(), five_el.end());
-        midl.push_back(five_el[2]);
+            five_el[j] = A[left + 5 * i + j];
+    	}
+        midl.push_back(med(five_el, 5));
     }
     five_el.resize(the_whole_part);
     for(int j = 0; j < the_whole_part; ++j){
-        five_el[j] = A[A.size() - the_whole_part + j];
+        five_el[j] = A[right + 1 - the_whole_part + j];
     }
-    sort(five_el.begin(), five_el.end());
-    midl.push_back(five_el[five_el.size() / 2]);
-    return midl;
+	midl.push_back(med(five_el, the_whole_part));
+    return kth(midl, 0, midl.size() - 1);
 }
 
-void Partition(int mid, std::vector<int>& A, std::vector<int>& mid_less, std::vector<int>& mid_equally, std::vector<int>& mid_more){
-     for(int i = 0; i < A.size(); ++i){
-        if(A[i] < mid){
-            mid_less.push_back(A[i]);
-        }
-        else if(A[i] == mid){
-            mid_equally.push_back(A[i]);
-        }
-        else{
-            mid_more.push_back(A[i]);
-        }
+void Partition(int mid, std::vector<int>& A,int& equall, int& left, int& right){
+	if(left - right == 1){
+		return;
+	}
+    int left1 = left; 
+	while(left != right){
+         if(A[left] > mid){
+		 	std::swap(A[left], A[right]);
+			right--;
+		 }
+		 else{
+			left++;
+		 }
     }
+	if(A[right] > mid){
+		left--;
+	}
+	else{
+		right++;
+	}
+	while(left != left1){
+		if(A[left1] == mid){
+			std::swap(A[left], A[left1]);
+			left--;
+			equall++;
+		}
+		else{
+			left1++;
+		}
+	}
+	if(A[left] == mid){
+		left--;
+		equall++;
+	}
 }
 
 int QuickSelect(std::vector<int>& A,int k){
     if(A.size() == 1){
     	return A[0];
     }
-    std::vector<int> midl = kth(A);
-    int mid = QuickSelect(midl, midl.size() / 2);;
-    std::vector<int> mid_less, mid_equally, mid_more;
-    Partition(mid, A, mid_less, mid_equally, mid_more);
-    if(k < mid_less.size()){
-    	return QuickSelect(mid_less, k);
-    }
-    else if(k < mid_less.size() + mid_equally.size()){
-        return mid_equally[0];
-    }
-    else{
-    	return QuickSelect(mid_more, k - mid_less.size() - mid_equally.size());
-    }
+    
+    int left = 0, right = A.size() - 1;
+	while(left != right){
+		int mid = kth(A, left, right);
+		int left1 = left;
+		int right1 = right;
+		int equall = 0;
+		Partition(mid, A, equall, left, right);
+		if(left + 1 >= k){
+			right = left;
+			left = left1;
+		}
+		else if(left + equall + 1 >= k){
+			return A[left + equall];
+		}
+		else{
+			left = right;
+			right = right1;
+		}
+	}
+	return A[left];
 }
-
 int main(){
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(0);
